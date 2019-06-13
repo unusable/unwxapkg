d5bae7b7ac4ba0f0090d7995d699e56715090e7c
unusable <unusable@163.com>
2019-06-13 10:19:34 +0800

    change output dir base from workdir

--- bin/cmd.js
+++ bin/cmd.js
@@ -12,7 +12,7 @@ let destination = process.argv[3]? process.argv[3] : 'wxapkg.unpack';
 let wxapkg = new Wxapkg(file);
 let files = wxapkg.decode();
 files.forEach((f)=>{
-    let filePath = __dirname + '/./' + destination + f.name;
+    let filePath = path.join(destination, f.name);
     let dir = path.dirname(filePath);
     mkdirp(dir, function (err) {
         if (err) return cb(err);
